3647faa44907cdd153e6223d78653fc69328a39f
hujinshou <hujinshou@feibotong.com>
2016-10-03 12:35:59 +0800

    update hongbao.js

--- app/js/hongbao.js
+++ app/js/hongbao.js
@@ -1,6 +1,8 @@
+var $ = require('jquery');
 $(function(){
     var obj = $('#get-hb');
     obj.on('touchstart click', function() {
+        alert('ok');
         $(this).toggleClass('hover');
     }); 
 });
